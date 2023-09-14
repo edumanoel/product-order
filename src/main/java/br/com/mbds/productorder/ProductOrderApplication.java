75ff6adc7a0106070883bffe6eee3586a26e0783
Eduardo Belem <leal@pm.ba.gov.br>
2023-09-14 15:52:07 -0300

    Project creation

---
+++ src/main/java/br/com/mbds/productorder/ProductOrderApplication.java
@@ -0,0 +1,13 @@
+package br.com.mbds.productorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mbds/productorder/ProductOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mbds.productorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
