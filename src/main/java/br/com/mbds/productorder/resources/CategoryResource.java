0bf8c15fed11e665d984d498e1db9e943be0f567
Eduardo Belem <leal@pm.ba.gov.br>
2023-09-28 19:08:12 -0300

    created insert, delete and update category

--- src/main/java/br/com/mbds/productorder/resources/CategoryResource.java
+++ src/main/java/br/com/mbds/productorder/resources/CategoryResource.java
@@ -1,13 +1,19 @@
 package br.com.mbds.productorder.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.mbds.productorder.entities.Category;
 import br.com.mbds.productorder.services.CategoryService;
@@ -28,6 +34,24 @@ public class CategoryResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Category> findById(@PathVariable Long id) {
 		return ResponseEntity.ok().body(service.findById(id));
+	}
+
+	@PostMapping
+	public ResponseEntity<Category> insert(@RequestBody Category obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Category> update(@PathVariable Long id, @RequestBody Category obj) {
+		return ResponseEntity.ok().body(service.update(id, obj));
 	}
 
 }
--- src/main/java/br/com/mbds/productorder/services/CategoryService.java
+++ src/main/java/br/com/mbds/productorder/services/CategoryService.java
@@ -3,11 +3,14 @@ package br.com.mbds.productorder.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.mbds.productorder.entities.Category;
 import br.com.mbds.productorder.repositories.CategoryRepository;
+import br.com.mbds.productorder.services.exceptions.DatabaseException;
 import br.com.mbds.productorder.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -21,6 +24,33 @@ public class CategoryService {
 
 	public Category findById(Long id) {
 		return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+
+	public Category insert(Category obj) {
+		return repository.save(obj);
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+
+	public Category update(Long id, Category obj) {
+		try {
+			Category entity = repository.getReferenceById(id);
+			setData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+
+	}
+
+	private void setData(Category entity, Category obj) {
+		entity.setName(obj.getName());
 	}
 
 }
