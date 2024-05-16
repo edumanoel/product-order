55c2872ea45fb1a7b50456ecb570583272e53f98
Eduardo Belem <edumanoel@gmail.com>
2024-05-16 14:57:42 -0300

    FIX: set type OrderItemPk in OrderItemRepository

--- src/main/java/br/com/mbds/productorder/repositories/OrderItemRepository.java
+++ src/main/java/br/com/mbds/productorder/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package br.com.mbds.productorder.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.mbds.productorder.entities.OrderItem;
+import br.com.mbds.productorder.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 
 }
